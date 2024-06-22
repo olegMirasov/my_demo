af7d70ff21c7dcd82959d8736248443796be14a4
OlegMirasov <oleg.mirasov@mail.ru>
2024-06-22 07:42:21 +0300

    Немного поменял функцию загрузки данных в power bi

--- deal_for_powerbi/views/export_deals_view.py
+++ deal_for_powerbi/views/export_deals_view.py
@@ -10,23 +10,30 @@ def export_deals(request):
     Функция отвечает за сбор данных из битрикса, обработку в нужный вид и
     отправку в PowerBI.
     """
+    # https://apparently-endless-wren.ngrok-free.app/deal_for_powerbi/export_deals/?key=DoiyHseg8yuJBChuibaKa
 
-    if request.its_params.get('s') != 'KJBHILiswbeg8yuesbg':
+    if request.its_params.get('key') != 'DoiyHseg8yuJBChuibaKa':
         return HttpResponseForbidden()
     but = BitrixUser.objects.filter(is_admin=True,
                                     user_is_active=True).first().bitrix_user_token
 
-    deals = but.call_list_method('crm.deal.list', {
-        'select': ['ASSIGNED_BY_ID', 'COMPANY_ID', 'CONTACT_IDS',
-                   'DATE_CREATE', 'TITLE']})
-    companies = but.call_list_method('crm.company.list', {
-        'select': ['ADDRESS', 'ADDRESS_CITY', 'ID', 'LEAD_ID', 'TITLE']})
-    contacts = but.call_list_method('crm.contact.list', {
-        'select': ['ID', 'LEAD_ID', 'NAME', 'LAST_NAME', 'COMPANY_IDS']})
+    deals = but.call_list_method('crm.deal.list',
+                                 {'select': ['ASSIGNED_BY_ID', 'COMPANY_ID', 'CONTACT_IDS', 'DATE_CREATE', 'TITLE']})
+
+    companies = but.call_list_method('crm.company.list',
+                                     {'select': ['ADDRESS', 'ADDRESS_CITY', 'ID', 'LEAD_ID', 'TITLE']})
+
+    contacts = but.call_list_method('crm.contact.list',
+                                    {'select': ['ID', 'LEAD_ID', 'NAME', 'LAST_NAME', 'COMPANY_IDS']})
+
+    leads = but.call_list_method('crm.lead.list',
+                                 {'select': ["ID", "TITLE", "STATUS_ID", "OPPORTUNITY", "CURRENCY_ID"]})
+
     result = {
         'deals': deals,
         'companies': companies,
-        'contacts': contacts
+        'contacts': contacts,
+        'leads': leads
     }
 
     return JsonResponse(result, safe=False)
